f9f4dc749ea26e4e4d02ec554b310e0e88ec2f0a
Jonathan Tan <jhow.tan@gmail.com>
2015-03-14 16:13:37 +0800

    Set up logging for profile, modules; workbin in progress

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 var _ = require('lodash');
+var request = require('request');
 var Promise = require('bluebird');
 
+
 var config = require('./config');
+var ivle = require('./ivle');
+
+var _request = function _request(url, callback){
+  request(url, function(error, response, body) {
+    var results = body;
+    callback(error, results);
+  });
+};
+
+
+
+var _logProfile = function(token){
+  _request(ivle.profileUrl(token), function(err, body) {
+    console.log(err);
+    console.log(body);
+  });
+};
+
+var _logModules = function(token){
+  _request(ivle.modulesUrl(token), function(err, body) {
+    console.log(err);
+    console.log(body);
+  });
+};
+
+var _logWorkbin = function(token){
+    _request(ivle.modulesUrl(token), function(err, body) {
+      var modules = body.match(/"ID":"[^"]*"/gi);
+      modules = _.map(modules, function(moduleString) {
+        var modStringMatch = moduleString.match(/"ID":"([^"]*)"/i);
+        return modStringMatch[1];
+      });
+      console.log(modules);
+  });
+};
+
 
 function startLogging(token){
+//  _logProfile(token);
+//  _logModules(token);
+  _logWorkbin(token);
+  // console.log(ivle.modulesUrl(token));
 }
 
 if(!module.parent){
@@ -22,3 +64,27 @@ if(!module.parent){
     server.singleLoginServer().then(startLogging);
   }
 }
+
+// camelCase keys returned from IVLE Api
+var _camelCaseDeep = function _camelCaseDeep(body) {
+  if (_.isArray(body))
+    return _camelCaseArray(body);
+  else if (_.isPlainObject(body))
+    return _camelCaseObject(body);
+  else
+    return body;
+};
+
+var _camelCaseObject = function _camelCaseObject(obj){
+  return _(obj).pairs().map(function(pair){
+    var value = pair[1];
+    return [_.camelCase(pair[0]), _camelCaseDeep(value)];
+  }).object().value();
+};
+
+var _camelCaseArray = function _camelCaseArray(arr) {
+  return _(arr).map(function(value) {
+    return _camelCaseDeep(value);
+  }).value();
+};
+
