032d7943b25ee177f787d9f3f64d5448ca972259
Jonathan Tan <jhow.tan@gmail.com>
2015-03-14 16:15:34 +0800

    Set up logging for profile, modules; workbin in progress

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 var _ = require('lodash');
+var request = require('request');
 var Promise = require('bluebird');
 
 var config = require('./config');
+var ivle = require('./ivle');
+
+var _request = function _request(url, callback){
+  request(url, function(error, response, body) {
+    var results = body;
+    callback(error, results);
+  });
+};
+
+var _logProfile = function(token){
+  _request(ivle.profileUrl(token), function(err, body) {
+    console.log(err);
+    console.log(body);
+  });
+};
+
+var _logModules = function(token){
+  _request(ivle.modulesUrl(token), function(err, body) {
+    console.log(err);
+    console.log(body);
+  });
+};
+
+var _logWorkbin = function(token){
+    _request(ivle.modulesUrl(token), function(err, body) {
+      var modules = body.match(/"ID":"[^"]*"/gi);
+      modules = _.map(modules, function(moduleString) {
+        var modStringMatch = moduleString.match(/"ID":"([^"]*)"/i);
+        return modStringMatch[1];
+      });
+      console.log(modules);
+  });
+};
+
 
 function startLogging(token){
+//  _logProfile(token);
+//  _logModules(token);
+  _logWorkbin(token);
+  // console.log(ivle.modulesUrl(token));
 }
 
 if(!module.parent){
@@ -22,3 +61,4 @@ if(!module.parent){
     server.singleLoginServer().then(startLogging);
   }
 }
+
